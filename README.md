2d373c3bca1fd2dddb716dc57e45e1e4930efe93
beck-8 <1504068285@qq.com>
2026-04-02 19:46:50 +0800

    add install.sh ghproxy

--- README.md
+++ README.md
@@ -53,7 +53,7 @@ bash <(curl -fsSL https://raw.githubusercontent.com/beck-8/subs-check/master/ins
 bash <(wget -qO- https://raw.githubusercontent.com/beck-8/subs-check/master/install.sh)
 
 # 如果无法访问 GitHub，可使用代理
-bash <(curl -fsSL https://ghfast.top/https://raw.githubusercontent.com/beck-8/subs-check/master/install.sh)
+bash <(curl -fsSL https://ghfast.top/https://raw.githubusercontent.com/beck-8/subs-check/master/install.sh) https://ghfast.top/
 
 # Alpine 等无 bash 环境
 wget -qO /tmp/install.sh https://raw.githubusercontent.com/beck-8/subs-check/master/install.sh && sh /tmp/install.sh && rm -f /tmp/install.sh
--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 # 兼容 bash / sh / dash
 # 用法: curl -fsSL https://raw.githubusercontent.com/beck-8/subs-check/master/install.sh | bash
 #   或: wget -qO- https://raw.githubusercontent.com/beck-8/subs-check/master/install.sh | bash
+# 加速: bash <(curl -fsSL https://ghfast.top/https://raw.githubusercontent.com/beck-8/subs-check/master/install.sh) https://ghfast.top/
 
 set -e
 
@@ -13,7 +14,7 @@ BINARY_NAME="subs-check"
 SERVICE_NAME="subs-check"
 SERVICE_FILE="/etc/systemd/system/${SERVICE_NAME}.service"
 GITHUB_API="https://api.github.com/repos/${REPO}/releases/latest"
-GITHUB_PROXY=""
+GITHUB_PROXY="${1:-}"
 
 # ============ 运行状态 ============
 HAS_SYSTEMD=1
